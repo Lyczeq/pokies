e67d911cc2e439ed928d584c18f504e669d91c8f
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-03-07 15:35:11 +0100

    feat: add manipulating current location

--- hooks/useLocation.ts
+++ hooks/useLocation.ts
@@ -1,8 +1,54 @@
 import * as Location from 'expo-location';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
+import { Region } from 'react-native-maps';
+
+const initialLocation = {
+  longitude: 0,
+  latitude: 0,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+const handleLocationState = (state: Region, action) => {
+  switch (action.type) {
+    case 'CURRENT_LOCATION': {
+      return {
+        ...state,
+        latitude: action.latitude,
+        longitude: action.longitude,
+      };
+    }
+    case 'INCREMENT_LATITUDE': {
+      return {
+        ...state,
+        latitude: state.latitude + 0.005,
+      };
+    }
+    case 'DECREMENT_LATITUDE': {
+      return {
+        ...state,
+        latitude: state.latitude - 0.005,
+      };
+    }
+    case 'INCREMENT_LONGITUDE': {
+      return {
+        ...state,
+        longitude: state.longitude + 0.005,
+      };
+    }
+    case 'DECREMENT_LONGITUDE': {
+      return {
+        ...state,
+        longitude: state.longitude - 0.005,
+      };
+    }
+    default:
+      throw Error('Unknown action: ' + action.type);
+  }
+};
 
 export const useLocation = () => {
-  const [location, setLocation] = useState(null);
+  const [location, dispatch] = useReducer(handleLocationState, initialLocation);
   const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
@@ -14,17 +60,16 @@ export const useLocation = () => {
       }
 
       const location = await Location.getCurrentPositionAsync({});
-
-      setLocation({
+      dispatch({
+        type: 'CURRENT_LOCATION',
         longitude: location.coords.longitude,
         latitude: location.coords.latitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
       });
     })();
   }, []);
 
   return {
+    dispatch,
     errorMessage,
     location,
   };
--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,3 +1,5 @@
+import { Ionicons } from '@expo/vector-icons';
+import { useRef } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useGetSinglePokemon } from '../hooks/useGetSinglePokemon';
@@ -6,16 +8,48 @@ import { useLocation } from '../hooks/useLocation';
 const PIKACHU_URL = 'https://pokeapi.co/api/v2/pokemon/pikachu';
 
 export function Map() {
-  const { location, errorMessage } = useLocation();
-  const { pokemon, isLoading } = useGetSinglePokemon(PIKACHU_URL);
+  const { location, dispatch } = useLocation();
+  const { pokemon } = useGetSinglePokemon(PIKACHU_URL);
+  const mapRef: React.MutableRefObject<MapView> = useRef();
   const imageUrl = pokemon?.sprites['front_default'];
+
+  const jumpToCurrentLocation = () => {
+    mapRef.current.animateToRegion(location);
+  };
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.mapView} initialRegion={location}>
+      <MapView style={styles.mapView} region={location} showsMyLocationButton ref={mapRef}>
         <Marker coordinate={location} style={styles.marker}>
           <Image source={{ uri: imageUrl }} style={styles.image} />
         </Marker>
+        <View style={styles.iconsContainer}>
+          <Ionicons name="locate-outline" size={64} color="grey" onPress={jumpToCurrentLocation} />
+          <Ionicons
+            name="arrow-up"
+            size={32}
+            color="black"
+            onPress={() => dispatch({ type: 'INCREMENT_LATITUDE' })}
+          />
+          <Ionicons
+            name="arrow-forward"
+            size={32}
+            color="black"
+            onPress={() => dispatch({ type: 'INCREMENT_LONGITUDE' })}
+          />
+          <Ionicons
+            name="arrow-back"
+            color="black"
+            size={32}
+            onPress={() => dispatch({ type: 'DECREMENT_LONGITUDE' })}
+          />
+          <Ionicons
+            name="arrow-down"
+            color="black"
+            size={32}
+            onPress={() => dispatch({ type: 'DECREMENT_LATITUDE' })}
+          />
+        </View>
       </MapView>
     </View>
   );
@@ -29,6 +63,11 @@ const styles = StyleSheet.create({
     width: '15%',
     height: '15%',
   },
+  iconsContainer: {
+    alignSelf: 'flex-end',
+    marginBottom: 15,
+    marginLeft: 15,
+  },
   image: {
     width: '100%',
     height: '100%',
@@ -36,5 +75,8 @@ const styles = StyleSheet.create({
   mapView: {
     width: '100%',
     height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
   },
 });
